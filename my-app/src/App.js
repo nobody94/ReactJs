414a67bb7faccec5de18d601eb9a325763e6c594
94nobody <ngochuyen211294@gmail.com>
2020-02-16 13:40:13 +0700

    minicart

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,55 +9,69 @@ import SignIn from './pages/sign-in/Sign-in';
 import logo from './assets/crown.svg';
 import cartIcon from './assets/shopping-bag.svg';
 
-function App() {
-  const noItemMsg = "You had no item in your shopping cart";  
-   
-  return (
-    <Router>
-    <div className="project">
-        <header className="header">
-          <div className="container">
-            <Link to="/" className="logo"><img src={logo}></img></Link>
-            <nav className="nav-section">
-              <ul className="navigation">          
-                <li><Link to="/shop">Shop</Link></li>   
-                <li><Link to="/contact">Contact</Link></li>
-                <li><Link to="/sign-in">Sign in</Link></li> 
-                <li>
-                  <div className="minicart">
-                    <span className="minicart-icon">
-                      <img src={cartIcon}></img><span className="minicart-number">0</span>
-                    </span>
-                    <div className="minicart-content">
-                      <div className="block-content">
-                        <p className="message">{noItemMsg}</p>
-                      </div>
-                      <Link to="/" className="action">go to Checkout</Link>
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+       minicartIsOn : false,
+    }    
+    this.minicartClick = this.minicartClick.bind(this);
+  }
+  minicartClick(){
+    this.setState({
+      minicartIsOn : !this.state.minicartIsOn
+    })
+  }
+   render(){       
+    const noItemMsg = <p className="message">You had no item in your shopping cart</p>; 
+    let isOn = "minicart-content" + (this.state.minicartIsOn ?  " active" : "");    
+    return (
+      <Router>
+      <div className="project">
+          <header className="header">
+            <div className="container">
+              <Link to="/" className="logo"><img src={logo}></img></Link>
+              <nav className="nav-section">
+                <ul className="navigation">          
+                  <li><Link to="/shop">Shop</Link></li>   
+                  <li><Link to="/contact">Contact</Link></li>
+                  <li><Link to="/sign-in">Sign in</Link></li> 
+                  <li>
+                    <div className="minicart">
+                      <span className="minicart-icon" onClick={this.minicartClick}>
+                        <img src={cartIcon}></img><span className="minicart-number">0</span>
+                      </span>
+                      <div className={isOn}>
+                        <div className="block-content">
+                         {noItemMsg}
+                        </div>
+                        <Link to="/" className="action">go to Checkout</Link>
+                      </div> 
                     </div> 
-                  </div> 
-                </li>  
-              </ul>
-            </nav>
-          </div>         
-        </header>
-       
-      <Switch>
-        <Route exact path="/">
-          <Home></Home>
-        </Route>
-        <Route path="/contact">
-          <Contact></Contact>
-        </Route>
-        <Route path="/shop">
-          <Shop></Shop>
-        </Route>
-        <Route path="/sign-in">
-         <SignIn></SignIn>
-        </Route>
-      </Switch>
-    </div>
-  </Router>
-  );
+                  </li>  
+                </ul>
+              </nav>
+            </div>         
+          </header>
+         
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/sign-in">
+           <SignIn></SignIn>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    );
+   } 
 }
 
 export default App;
