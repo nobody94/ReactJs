c363c15f86794e0be99567c597213ac7051c682f
94nobody <ngochuyen211294@gmail.com>
2020-03-15 15:43:56 +0700

    done collection page

--- my-app/src/components/home/HomeItem.js
+++ my-app/src/components/home/HomeItem.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import { withRouter,Link} from 'react-router-dom';
 
-// class HomeItem extends React.Component{
-//  render(){
-//   const item = 'item' + (this.props.size =='large'? ' large':''); 
-//   return (
-//     <div className={item} >
-//       <Link className="banner" to={`/shop/${this.props.title}`}>
-//         <img src={this.props.imageUrl}></img>
-//         <div className="content">
-//           <h3 className="title">{this.props.title}</h3>
-//           <span className="sub-title">Shop now</span>
-//         </div>
-//       </Link>
-//     </div>
-//   );
-//  }
-// }
 const HomeItem = (props,{match}) =>{
   const item = 'item' + (props.size =='large'? ' large':''); 
+  const link = props.match.url;
+  console.log(link);
     return (
       <div className={item} >
-        <Link className="banner" /*to={`${match.url}${props.linkUrl}`}*/ >
+        <Link className="banner" to={`${link}${props.linkUrl}`} >
           <img src={props.imageUrl}></img>
           <div className="content">
             <h3 className="title">{props.title}</h3>
--- my-app/src/pages/collection/Collection.js
+++ my-app/src/pages/collection/Collection.js
@@ -2,47 +2,23 @@ import React from 'react';
 import {connect} from 'react-redux';
 import CollectionBlock from '../../components/collection/CollectionComponent';
 
-// class Collection extends React.Component {
-//   constructor(props){
-//     super(props);  
-   
-// } 
-//   ListItem(){
-//     return this.props.data.map((item)=>{
-//       return(
-//         <CollectionBlock key={item.id} title={item.title} product={item.items}></CollectionBlock> 
-//       )
-//     });
-//   }  
-//   render(){     
-//     return (      
-//       <div className="collection-view">
-//       {this.ListItem()}
-//       </div>
-//     );
-//   }
-// }
-// function mapStateToProps(state){
-//   return{
-//     data : state.shopData,
-//   }    
-// }
-// export default connect(mapStateToProps)(Collection);
-
-
  const Collection = (props,{match}) =>{
-  // return props.data.map((item)=>{
-  //   if(parseInt(match.params.shopId) === item.id){
-  //     return(
-  //       <CollectionBlock key={item.id} title={item.title} product={item.items}></CollectionBlock> 
-  //     )
-  //   }
-  //  })
-  // const link = props.data.find((cate)=>{
-  //   return parseInt(match.params.shopId) === cate.id
-  // });
-  // console.log(link);
-  return(<div></div>);
+  const shopId =  props.match.params.shopId;
+  console.log(shopId);
+  const shopItem = props.data.find((item)=>{
+    return shopId == item.routeName
+  });
+  console.log(shopItem);
+  const listItem = [shopItem].map((item)=>{
+      return(
+          <CollectionBlock key={item.id} title={item.title} product={item.items}></CollectionBlock> 
+        )
+     });
+  return(
+  <div className="collection-view">
+    {listItem}
+  </div>
+  );
  }
  function mapStateToProps(state){
     return{
