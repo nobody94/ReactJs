798c3b551ffb79b543f3661a5efd34eb3b399d79
94nobody <ngochuyen211294@gmail.com>
2020-03-17 16:54:11 +0700

    remove account success

--- my-app/src/components/user/User-detail.js
+++ my-app/src/components/user/User-detail.js
@@ -18,6 +18,7 @@ class Detail extends React.Component{
         this.onChangeHandle = this.onChangeHandle.bind(this);
         this.saveInformation = this.saveInformation.bind(this);
         this.changeInformation = this.changeInformation.bind(this);
+        this.deleteAccount = this.deleteAccount.bind(this);
       }
     componentDidMount(){
         firebase.auth().onAuthStateChanged(user => {
@@ -37,20 +38,26 @@ class Detail extends React.Component{
                     })
                 }
                 this.props.isSignIn();
-                firebase.database().ref('users/' + currentUser.uid).on('value', function(snapshot) {
-                    const accountInfo = snapshot.val();
-                  // console.log(snapshot.val());
-                  currentComponent.setState({
-                      address:accountInfo.address,
-                      phoneNumber:accountInfo.phoneNumber
-                  })
-                  if(currentComponent.state.address.length>0 && currentComponent.state.phoneNumber.length>0){
-                   currentComponent.setState({
-                    isAddress:true,
-                    isPhoneNumber:true,
-                   })
-                  }
-                });    
+                if(currentUser.uid){
+                    firebase.database().ref('users/' + currentUser.uid).on('value', function(snapshot) {
+                        const accountInfo = snapshot.val();
+                      // console.log(snapshot.val());
+                      if(accountInfo.address && accountInfo.phoneNumber){
+                        currentComponent.setState({
+                            address:accountInfo.address,
+                            phoneNumber:accountInfo.phoneNumber
+                        })
+                      }
+                      if(currentComponent.state.address.length>0 && currentComponent.state.phoneNumber.length>0){
+                       currentComponent.setState({
+                        isAddress:true,
+                        isPhoneNumber:true,
+                        address:accountInfo.address,
+                        phoneNumber:accountInfo.phoneNumber
+                       })
+                      }
+                    });  
+                }                 
             } else {
                 this.props.isLogOut();
             }
@@ -106,6 +113,20 @@ class Detail extends React.Component{
             }          
         });
     }
+    deleteAccount(){
+        firebase.auth().onAuthStateChanged(user => {
+            if(user) { 
+              const currentUser =  firebase.auth().currentUser;
+              firebase.database().ref('users/' + currentUser.uid).remove(); 
+              currentUser.delete().then(function(){
+                console.log('delete account success');
+                window.location = `/sign-in`;  
+              }).catch(function(error){
+                console.log('Delete account: '+error);
+              })         
+            }          
+        });
+    }
    render(){
    
     return(
@@ -115,21 +136,24 @@ class Detail extends React.Component{
                 <h4>Account Information</h4>
                 {
                     this.state.userName 
-                    ? <p>Username:{this.state.userName}</p>
+                    ? <p>Username: {this.state.userName}</p>
                     :null
                 }
-                <p>Email:{this.state.email}</p>
+                <p>Email: {this.state.email}</p>
+                <div className="btn-actions">
+                        <button className="action delete" onClick={this.deleteAccount}>Delete Account</button>
+                </div>
             </div>
            <div className="content">               
                 <h4>More Information</h4>
                 {
                  this.state.isAddress
-                    ? <p>{this.state.address}</p>
+                    ? <p>Address: {this.state.address}</p>
                  : <Input name="address" value={this.state.address} onChange={this.onChangeHandle} label="Address" required></Input>
                 }
                {
                 this.state.isPhoneNumber
-                ?<p>{this.state.phoneNumber}</p>
+                ?<p>Phone number: {this.state.phoneNumber}</p>
                 : <Input name="phoneNumber" value={this.state.phoneNumber} onChange={this.onChangeHandle} label="Phone Number" required></Input>
                }
                {
@@ -140,10 +164,10 @@ class Detail extends React.Component{
                {
                    this.state.isAddress && this.state.isPhoneNumber
                    ?   <div className="btn-actions">
-                   <button className="action submit" onClick={this.changeInformation}>Change Information</button>
+                   <button className="action change" onClick={this.changeInformation}>Change Information</button>
                     </div>
                         :  <div className="btn-actions">
-                        <button className="action submit" onClick={this.saveInformation}>Save</button>
+                        <button className="action save" onClick={this.saveInformation}>Save</button>
                     </div>
                }    
            </div>
